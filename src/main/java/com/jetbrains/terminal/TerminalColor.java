7bd81d2ab8a7e9529b81058ddd3d8802f13eb311
spasic4 <vasa.spasic@gmail.com>
2026-02-26 23:52:00 +0100

    Implement ANSI to TerminalColor

--- src/main/java/com/jetbrains/terminal/TerminalColor.java
+++ src/main/java/com/jetbrains/terminal/TerminalColor.java
@@ -7,5 +7,27 @@ public enum TerminalColor {
     DEFAULT,
     BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE,
     BRIGHT_BLACK, BRIGHT_RED, BRIGHT_GREEN, BRIGHT_YELLOW,
-    BRIGHT_BLUE, BRIGHT_MAGENTA, BRIGHT_CYAN, BRIGHT_WHITE
+    BRIGHT_BLUE, BRIGHT_MAGENTA, BRIGHT_CYAN, BRIGHT_WHITE;
+
+    public static TerminalColor fromAnsi(int code) {
+        return switch (code) {
+            case 30, 40 -> BLACK;
+            case 31, 41 -> RED;
+            case 32, 42 -> GREEN;
+            case 33, 43 -> YELLOW;
+            case 34, 44 -> BLUE;
+            case 35, 45 -> MAGENTA;
+            case 36, 46 -> CYAN;
+            case 37, 47 -> WHITE;
+            case 90, 100 -> BRIGHT_BLACK;
+            case 91, 101 -> BRIGHT_RED;
+            case 92, 102 -> BRIGHT_GREEN;
+            case 93, 103 -> BRIGHT_YELLOW;
+            case 94, 104 -> BRIGHT_BLUE;
+            case 95, 105 -> BRIGHT_MAGENTA;
+            case 96, 106 -> BRIGHT_CYAN;
+            case 97, 107 -> BRIGHT_WHITE;
+            default -> DEFAULT;
+        };
+    }
 }
